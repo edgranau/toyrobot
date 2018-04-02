616c63250cda96352a99e7d1a709166527323feb
Edwin Granados <edgranau@gmail.com>
2018-04-02 23:42:43 +1000

    add MOVE command

--- lib/toyrobot/robot.rb
+++ lib/toyrobot/robot.rb
@@ -12,6 +12,15 @@ module Toyrobot
     def report
       "#{@x},#{@y},#{@direction}"
     end
+
+    def move
+      case @direction
+      when "NORTH" then @y += 1
+      when "SOUTH" then @y -= 1
+      when "EAST" then @x += 1
+      when "WEST" then @x -= 1
+      end
+    end
   
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 RSpec.describe Toyrobot::Robot do
-  context("before a valid PLACE command") do
+  context("Before a valid PLACE command") do
     it "shows the robot's default position" do
       expect(subject.report).to eql "-1,-1,N/A"
     end
@@ -15,5 +15,41 @@ RSpec.describe Toyrobot::Robot do
       expect(subject.report).to eql "0,0,NORTH"
     end
   
+  end
+
+  context("when facing NORTH") do
+    subject { Toyrobot::Robot.new(0,0,"NORTH") }
+
+    it "moves NORTH" do
+      subject.move
+      expect(subject.report).to eql "0,1,NORTH"
+    end
+  end
+
+  context("when facing SOUTH") do
+    subject { Toyrobot::Robot.new(0,1,"SOUTH") }
+
+    it "moves SOUTH" do
+      subject.move
+      expect(subject.report).to eql "0,0,SOUTH"
+    end
+  end
+
+  context("when facing EAST") do
+    subject { Toyrobot::Robot.new(0,0,"EAST") }
+
+    it "moves EAST" do
+      subject.move
+      expect(subject.report).to eql "1,0,EAST"
+    end
+  end
+
+  context("when facing WEST") do
+    subject { Toyrobot::Robot.new(1,0,"WEST") }
+
+    it "moves WEST" do
+      subject.move
+      expect(subject.report).to eql "0,0,WEST"
+    end
   end
 end

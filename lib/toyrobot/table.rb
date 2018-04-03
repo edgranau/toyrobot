40715af45a50acde07585b71bbf308668726069f
Edwin Granados <edgranau@gmail.com>
2018-04-03 20:27:45 +1000

    improve readability

--- lib/toyrobot/table.rb
+++ lib/toyrobot/table.rb
@@ -7,8 +7,8 @@ module Toyrobot
     end
     
     def is_valid_position?(x,y)
-      x >= 0 && x <= @x &&
-      y >= 0 && y <= @y     
+      (0...@x).cover?(x) &&
+      (0...@y).cover?(y)
     end
   
   end

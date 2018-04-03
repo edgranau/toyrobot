637db463e62e9432245c3f6abe7e0ab98763cc68
Edwin Granados <edgranau@gmail.com>
2018-04-04 00:20:11 +1000

    cleanup

--- lib/toyrobot.rb
+++ lib/toyrobot.rb
@@ -1,7 +1,9 @@
-require "toyrobot/version"
+require "toyrobot/cli"
+require "toyrobot/command"
 require "toyrobot/robot"
 require "toyrobot/simulation"
 require "toyrobot/table"
+require "toyrobot/version"
 
 module Toyrobot
   # Your code goes here...

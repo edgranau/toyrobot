58a6051506a4da007d7eea3d9a8d07fe5e3e404c
Edwin Granados <edgranau@gmail.com>
2018-04-04 00:21:45 +1000

    add cli abstraction

---
+++ lib/toyrobot/cli.rb
@@ -0,0 +1,22 @@
+module Toyrobot
+  class CLI
+
+    def initialize(filename)
+      @filename = filename
+      @table = Table.new
+      @simulation = Simulation.new(@table)
+      @command = Command.new
+    end
+
+    def run
+      File.open(@filename,'r').each { |line| run_command line }
+    end
+
+    private
+
+    def run_command(line)
+      @simulation.send(*@command.parse(line))
+    end
+
+  end
+end
